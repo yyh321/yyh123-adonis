d2b0ded7675521641c637e8351ba7b39edc64940
yyh123 <yuyunhao1@126.com>
2019-03-27 16:09:32 +0800

    获取请求中的数据

--- start/routes.js
+++ start/routes.js
@@ -24,3 +24,38 @@ Route.on('/').render('welcome')
 Route.get('/posts', ({
   request
 }) => request.get())
+
+/**
+ * 获取body 主体参数
+ */
+Route.post('/posts1', ({
+  request
+}) => request.post())
+
+/**
+ * 同时获取查询参数和主体参数
+ */
+Route.post('/posts2', ({
+  request
+}) => request.all())
+
+/**
+ * 获取指定参数请求
+ */
+Route.post('/posts3', ({
+  request
+}) => request.only(['title', 'content']))
+
+/**
+ * 获取除了指定以外的请求参数
+ */
+Route.post('/posts4', ({
+  request
+}) => request.except(['title', 'content']))
+
+/**
+ * 获取指定参数的值，如果获取不到，第二个参数为默认值
+ */
+Route.post('/posts', ({
+  request
+}) => request.input('status', 'defaultValue'))
